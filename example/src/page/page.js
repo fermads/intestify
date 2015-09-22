780ab0d0f4fba51a72ea8f3785906371283a846a
Fernando Miçalli <fermads@gmail.com>
2015-09-22 02:22:55 -0300

    adding new test page

---
+++ example/src/page/page.js
@@ -0,0 +1,5 @@
+var Page = {
+  build: function() {
+    return 'ok';
+  }
+}
---
+++ example/test/spec/page/pageSpec.js
@@ -0,0 +1,7 @@
+describe('Page', () => {
+
+  it('Should say ok', () => {
+    expect(Page.build()).toEqual('ok');
+  });
+
+});
